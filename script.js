ba49172324c01d513d5acce32255e103ed0c589e
lynxsenja13 <lynxsenja13@gmail.com>
2026-03-01 22:51:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -493,3 +493,27 @@ function renderTabelGizi(total, kategori) {
     </table>
   `;
 }
+
+function formatTanggalIndonesia() {
+  const hari = ["Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu"];
+  const bulan = [
+    "Januari","Februari","Maret","April","Mei","Juni",
+    "Juli","Agustus","September","Oktober","November","Desember"
+  ];
+
+  const now = new Date();
+
+  const namaHari = hari[now.getDay()];
+  const tanggal = now.getDate();
+  const namaBulan = bulan[now.getMonth()];
+  const tahun = now.getFullYear();
+
+  return `${namaHari}, ${tanggal} ${namaBulan} ${tahun}`;
+}
+
+function exportPDF() {
+  const title = document.getElementById("printTitle");
+  title.innerText = "Laporan Gizi Harian\n" + formatTanggalIndonesia();
+
+  window.print();
+}
