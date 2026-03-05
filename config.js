a9f26fb2f556a5638bc78f8a050be754cf867ca7
lynxsenja13 <lynxsenja13@gmail.com>
2026-03-06 05:35:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbxhf12i0BLsoJ9HNXA129hMXBhyjmXr52tpf6dgEpr2FBIyJcsJmy3RrDm4gEFPEAw-/exec";
-const API_URL2 = "https://script.google.com/macros/s/AKfycbxB_Em8FcPbgXZaFvFjd3fvplHpEMbv5jyRGQEZD9ygytBUq6qlMggIP40RpFi4Z8ds/exec";
+const API_URL2 = "https://script.google.com/macros/s/AKfycbznHYoLdRf92CiL4GDy-xBVhn3WqPa36XtvHswLP4ZJ-pBbokdBT5JHIi8Q0Y_JwY_w/exec";
