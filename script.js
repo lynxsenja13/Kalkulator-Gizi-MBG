f779612de609681d38f09c98d003005c12c21ce4
lynxsenja13 <lynxsenja13@gmail.com>
2026-04-10 05:43:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1932,21 +1932,19 @@ function kirimSpreadsheet() {
   };
 
   fetch(API_URL2, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    body: "data=" + encodeURIComponent(JSON.stringify(payload))
+  method: "POST",
+  mode: "no-cors", // 🔥 INI KUNCI
+  body: new URLSearchParams({
+    data: JSON.stringify(payload)
   })
-  .then(res => res.json())
-  .then(res => {
-    alert("✅ Berhasil dikirim!");
-    console.log(payload); // 🔥 DEBUG
-  })
-  .catch(err => {
-    console.error(err);
-    alert("❌ Gagal kirim");
-  });
+})
+.then(() => {
+  alert("✅ Berhasil dikirim!");
+})
+.catch(err => {
+  console.error(err);
+  alert("❌ Gagal kirim");
+});
   
 }
   
